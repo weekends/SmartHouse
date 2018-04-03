f4266e2be413204258ada897e45b9d7265a0607d
root <root@beaglebone.localdomain>
2018-04-03 11:04:25 +0000

    Add systemd script for autostart clients

--- configs/instsall_services.sh
+++ configs/instsall_services.sh
@@ -6,7 +6,7 @@ DEST_SYSTEMD_PATH='/lib/systemd/system'
 for f in $(ls -1 "$SCRIPT_DIR/etc/systemd/system/"); do
 	cp -a "$SCRIPT_DIR/etc/systemd/system/$f" "$DEST_SYSTEMD_PATH"
 	chown root.root "$DEST_SYSTEMD_PATH/$f"
-	chmod 755 "$DEST_SYSTEMD_PATH/$f"
+#	chmod 755 "$DEST_SYSTEMD_PATH/$f"
 	systemctl enable $f
 	systemctl restart $f
 done
