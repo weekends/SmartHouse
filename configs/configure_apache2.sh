3fecd0055b0fa2d309864d3bcdabd9bae781c863
root <root@beaglebone.localdomain>
2018-04-04 11:41:19 +0000

    apache2 configuration fixes

--- configs/configure_apache2.sh
+++ configs/configure_apache2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(dirname -- "$0")
-DEST_SYSTEMD_PATH='etc/apache2/sites-available/'
+DEST_SYSTEMD_PATH='/etc/apache2/sites-available/'
 
 for f in $(ls -1 "$SCRIPT_DIR/etc/apache2/sites-available/"); do
 	cp -a "$SCRIPT_DIR/etc/apache2/sites-available/$f" "$DEST_SYSTEMD_PATH"
@@ -11,7 +11,7 @@ done
 a2enmod fcgid
 a2ensite SmartHouse.conf
 
-grep -q 'Listen 4433' /etc/apache2/ports.conf || echo 'Listen 4433' >> /etc/apache2/ports.conf
+grep -q -e '^Listen 80$' /etc/apache2/ports.conf || echo 'Listen 80' >> /etc/apache2/ports.conf
 
 systemctl reload apache2
 systemctl enable apache2
